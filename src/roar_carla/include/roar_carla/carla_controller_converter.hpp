daf9997c4429e5df5a9103009f4896a3a59dd02f
Michael Wu <wuxiaohua1011@gmail.com>
2023-03-12 23:01:53 -0700

    lite: angle awareness

--- src/roar_carla/include/roar_carla/carla_controller_converter.hpp
+++ src/roar_carla/include/roar_carla/carla_controller_converter.hpp
@@ -19,6 +19,19 @@ namespace control_converter
         void auto_controller_callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg);
         void manual_controller_callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg);
         void drive_mode_callback(const std_msgs::msg::Bool::SharedPtr msg);
+        float map_range(float value, float orig_min, float orig_max, float target_min, float target_max)
+        {
+            // calculate the original range
+            float orig_range = orig_max - orig_min;
+
+            // calculate the target range
+            float target_range = target_max - target_min;
+
+            // scale the original value to the target range
+            return (((value - orig_min) * target_range) / orig_range) + target_min;
+        }
+        float ackermann_to_carla_steering(float ackermann_steering);
+        
         rclcpp::TimerBase::SharedPtr timer_;
         
         rclcpp::Publisher<carla_msgs::msg::CarlaEgoVehicleControl>::SharedPtr auto_carla_control_publisher_;
@@ -28,6 +41,12 @@ namespace control_converter
         rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr auto_controller_sub_;
         rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr manual_controller_sub_;
         rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr manual_control_override_sub_;
+
+        float CARLA_MAX_RIGHT_STEERING = 1.0;
+        float CARLA_MAX_LEFT_STEERING = -1.0;
+
+        float INPUT_MAX_RIGHT_STEERING = 1.0;
+        float INPUT_MAX_LEFT_STEERING = 1.0;
     };
 
 } // namespace control_converter
--- src/roar_carla/src/carla_controller_converter.cpp
+++ src/roar_carla/src/carla_controller_converter.cpp
@@ -6,6 +6,12 @@ namespace control_converter
 {
     ControlConverter::ControlConverter() : Node("controller_converter")
     {
+        this->declare_parameter("input_max_right_steering", 1.0);
+        this->declare_parameter("input_max_left_steering", -1.0);
+
+        this->INPUT_MAX_RIGHT_STEERING = this->get_parameter("input_max_right_steering").as_double();
+        this->INPUT_MAX_LEFT_STEERING = this->get_parameter("input_max_left_steering").as_double();
+
         // Initialize publisher
         auto_carla_control_publisher_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>("/carla/ego_vehicle/vehicle_control_cmd", 10);
         manual_carla_control_publisher_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>("/carla/ego_vehicle/vehicle_control_cmd_manual", 10);
@@ -36,7 +42,7 @@ namespace control_converter
     {
         carla_msgs::msg::CarlaEgoVehicleControl output;
         output.throttle = input->drive.jerk;
-        output.steer = input->drive.steering_angle_velocity;
+        output.steer = this->ackermann_to_carla_steering(input->drive.steering_angle_velocity);
         if (input->drive.speed < 0)
         {
             output.reverse = true;
@@ -45,7 +51,17 @@ namespace control_converter
             output.reverse = false;
         }
         return std::make_shared<carla_msgs::msg::CarlaEgoVehicleControl>(output);
-        
+    }
+
+    float ControlConverter::ackermann_to_carla_steering(float ackermann_steering)
+    {
+        return std::max(CARLA_MAX_LEFT_STEERING,
+                std::min(CARLA_MAX_RIGHT_STEERING,
+                this->map_range(ackermann_steering,
+                                CARLA_MAX_LEFT_STEERING,
+                                CARLA_MAX_RIGHT_STEERING,
+                                INPUT_MAX_LEFT_STEERING,
+                                INPUT_MAX_RIGHT_STEERING)));
     }
 }
 
